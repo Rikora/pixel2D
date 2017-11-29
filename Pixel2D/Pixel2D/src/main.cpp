41c2990a6e1f146cdacf309a92eb034fd53cb09a
JAKOB\Jakob <jakob.ahrneteg@gmail.com>
2017-11-30 00:22:13 +0100

    Added release version of SFML

--- Pixel2D/Pixel2D/src/main.cpp
+++ Pixel2D/Pixel2D/src/main.cpp
@@ -1,8 +1,25 @@
 #include "utils\Macros.hpp"
+#include <SFML\Graphics.hpp>
 
 int main()
 {
-	PRINT("Hello world")
-	system("pause");
-	return EXIT_SUCCESS;
+	sf::RenderWindow window(sf::VideoMode(600, 480), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
