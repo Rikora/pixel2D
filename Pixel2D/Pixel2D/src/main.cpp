10ba54abd7fb638b64725792ba46001ead21383e
JAKOB\Jakob <jakob.ahrneteg@gmail.com>
2017-11-30 15:17:38 +0100

    Added thor library

--- Pixel2D/Pixel2D/src/main.cpp
+++ Pixel2D/Pixel2D/src/main.cpp
@@ -1,5 +1,6 @@
 #include "utils\Macros.hpp"
 #include <SFML\Graphics.hpp>
+#include <Thor\Input.hpp>
 
 int main()
 {
@@ -7,14 +8,34 @@ int main()
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
+	//Thor
+	thor::ActionMap<std::string> map;
+	thor::ActionMap<std::string>::CallbackSystem system;
+
+	map["Quit"] = thor::Action(sf::Keyboard::Escape, thor::Action::PressOnce);
+	map["Shoot"] = thor::Action(sf::Keyboard::E, thor::Action::PressOnce);
+
+	//Callback
+	system.connect("Shoot", [](thor::ActionContext<std::string> context) { PRINT("Hello there")} );
+
 	while (window.isOpen())
 	{
+		//Clear events from last frame
+		map.clearEvents();
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			map.pushEvent(event);
+
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		map.invokeCallbacks(system, nullptr);
+
+		if (map.isActive("Quit"))
+			window.close();
 
 		window.clear();
 		window.draw(shape);
